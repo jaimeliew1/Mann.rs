48bf192b151a6d2c667527eeaed6493212547ceb
Jaime Liew <jaimeliew1@gmail.com>
2025-03-26 15:06:17 +0100

    fix mona lisa example

--- examples/example_06_monalisa.py
+++ examples/example_06_monalisa.py
@@ -12,17 +12,20 @@ FIGDIR = Path("fig")
 FIGDIR.mkdir(parents=True, exist_ok=True)
 
 IMAGE_FN = Path(__file__).parent / "mona_lisa.webp"
-
-ae, L, gamma = 0.2, 30, 3.9
-Lx, Ly, Lz = 100, 500, 500
-Nx, Ny, Nz = 100, 32, 32
-RES = 20
+# METHOD = "fastinterp"
+# METHOD = "python_reduced"
+METHOD = "rust"
+PARALLEL = True
+ae, L, gamma = 0.05, 30, 3.9
+Lx, Ly, Lz = 1000, 1000, 1000
+Nx, Ny, Nz = 128, 64, 64
+RES = 60
 if __name__ == "__main__":
     # Load mona lisa image as an array zero-mean array.
     img = Image.open(IMAGE_FN).resize((RES, RES)).convert("L")
     arr = np.array(img)
-    arr = (arr - arr.mean()) / np.std(arr) * 10
-
+    arr = (arr - arr.mean()) / np.std(arr) * 5
+    print(arr.min(), arr.max(), arr.mean())
 
     # Convert mona lisa pixels to wind field constraints.
     constraints = []
@@ -31,11 +34,26 @@ if __name__ == "__main__":
             constraints.append(Constraint(Lx / 2, y, z, arr[i, j], None, None))
 
     print(len(constraints))
-    stencil = ConstrainedStencil(constraints, ae, L, gamma, Nx, Ny, Nz, Lx, Ly, Lz, parallel=True)
-    print(stencil)
-    U, V, W = stencil.turbulence(1234)
+    stencil = ConstrainedStencil(
+        constraints,
+        ae,
+        L,
+        gamma,
+        Nx,
+        Ny,
+        Nz,
+        Lx,
+        Ly,
+        Lz,
+        parallel=PARALLEL,
+        aperiodic_x=False,
+    )
+    print(stencil.stencil)
+    U, V, W = stencil.turbulence(1234, method=METHOD, parallel=PARALLEL, thres=0.0005)
 
     for i, slice in enumerate(tqdm(U)):
+        if i % 16 != 0:
+            continue
         plt.figure()
         plt.imshow(slice)
         plt.savefig(FIGDIR / f"{i:03d}.png", dpi=300)
