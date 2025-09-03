2943088f083bfd8230c6e5495540f56a2f94270a
Jaime Liew <jaimeliew1@gmail.com>
2025-09-03 15:47:46 +0200

    fix import issue

--- mannrs/__init__.py
+++ mannrs/__init__.py
@@ -1,4 +1,4 @@
-from .Mann import Stencil, save_box, load_mann_binary, mann_spectra
+from .Mann import Stencil, load_mann_binary, mann_spectra
 from .Constrained import ConstrainedStencil, Constraint
 from .Windfield import Windfield
 
